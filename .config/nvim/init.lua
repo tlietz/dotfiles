43b30ad2d245e37675d3ecda73eeb2ddeece67fb
tlietz <theodore.lietz@gmail.com>
2024-03-11 23:59:36 -0500

    removes  as accepting autocomplete

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -439,10 +439,9 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        -- <C-i> should be recognized as <Tab>
+        -- <C-i> is recognized as <Tab>
         ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-k>'] = cmp.mapping.confirm({ select = true }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         ['<C-c>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
     }),
